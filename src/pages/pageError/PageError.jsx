4b897b04e0ebf30dcfb416473c52538add9a33d7
Thierry DE SOUSA <thierry.desousa77@gmail.com>
2024-09-14 10:53:38 +0200

    update about current

--- src/pages/pageError/PageError.jsx
+++ src/pages/pageError/PageError.jsx
@@ -1,9 +1,27 @@
+// VARIABLES
+import Error404 from '../../assets/images/404Error-amico.png'
+
+// STYLE
+import './pageError.scss'
+
 function PageError() {
     return (
         <main>
-            <section>
-
+            <section className='errorPageContainer'>
+                <div className='errorImgContainer'>
+                    <img 
+                        src={Error404} 
+                        alt="Erreur 404" 
+                        className='errorImgContainer__img'
+                    />
+                </div>
+                <div className='ErrorMessageContainer'>
+                    <p className='ErrorMessageContainer__text'>
+                        Oups ! La page que vous demandez n'existe pas.
+                    </p>
+                </div>
             </section>
+            
         </main>
     )
 }

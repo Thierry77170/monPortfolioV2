cbc20edd75f7268f244e593404a73314d69053d7
Thierry DE SOUSA <thierry.desousa77@gmail.com>
2024-09-24 15:08:45 +0200

    update Appli React

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -32,7 +32,7 @@ function About() {
                         className='aboutMeContainer__img'
                     />
                     <p className='aboutMeContainer__text'>
-                        Après avoir exploré différents horizons professionnels, j'ai décidé d'effectuer une reconversion professionnel dans le développement web, un domaine qui m'anime depuis longtemps.
+                        Après avoir exploré différents horizons professionnels, j'ai décidé d'effectuer une reconversion professionnelle dans le développement web, un domaine qui m'anime depuis longtemps.
                         Récemment diplômé en qualité de développeur intégrateur web, j'ai acquis une solide base de compétences techniques en HTML, CSS, JavaScript, React, Redux, et bien d'autres.
                         Mais au-delà de mes compétences techniques, je suis convaincu que c'est ma curiosité, mon esprit d'équipe et ma capacité à résoudre les problèmes qui font de moi un véritable atout pour toute entreprise en quête d'innovation et de qualité.
                         Je suis impatient de mettre mes compétences et ma passion au service de projets stimulants et de continuer à apprendre et à grandir dans ce domaine en constante évolution.

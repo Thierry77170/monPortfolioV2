ca7f86112a45de101ad466acab15e48e18b9f4cc
Thierry DE SOUSA <thierry.desousa77@gmail.com>
2024-09-26 18:16:19 +0200

    update ProjectCard.jsx

--- src/components/projectCard/ProjectCard.jsx
+++ src/components/projectCard/ProjectCard.jsx
@@ -1,5 +1,6 @@
 // VARIABLES
 import gitHub from '../../assets/icons/github.256x251.png';
+import web from '../../assets/icons/web.256x256.png';
 
 // STYLE
 import './projectCard.scss';
@@ -8,12 +9,24 @@ function ProjectCard({project, style, text}) {
   return (
     <div className='slide' style={style}>
         <img className='slide__img' src={project.image.stringValue} alt={`présentation de ${project.title.stringValue}`} />
-
         <h3 className='slide__title'>{project.title.stringValue}</h3>
         <p className='slide__text'>{project.text.stringValue}</p>
-        <a className='slide__lien' href={project.lienGitHub.stringValue} target="_blank" rel="noreferrer">
-          <img className='slide__lien--icon' src={gitHub} alt="icon de GitHub"/>
-        </a>
+        <div className='containerSlideLien'>
+          <a className='containerSlideLien__lien' href={project.lienGitHub.stringValue} target="_blank" rel="noreferrer">
+            <img className='containerSlideLien__lien--icon' src={gitHub} alt="icon de GitHub"/>
+          </a>
+          {project.lienWeb.stringValue && (
+            <a
+              className='containerSlideLien__lien'
+              href={project.lienWeb.stringValue}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img className='containerSlideLien__lien--icon' src={web} alt="icon de site web" />
+            </a>
+          )}
+        </div>
+       
     </div>
   );
 }
--- src/rooting/Root.jsx
+++ src/rooting/Root.jsx
@@ -1,23 +1,11 @@
-// REACT
-import React, { useEffect } from 'react';
-
-
 // REACT-ROUTER-DOM
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 // COMPONENTS
 import Header from '../components/header/Header.jsx'
 import Footer from '../components/footer/Footer.jsx'
 
 function Root() {
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        if (window.location.pathname !== '/home') {
-          navigate('/home'); // Redirige vers la page "home" lorsque la page est rafraîchie
-        }
-      }, [navigate]);
-
     return (
         <>
             <Header />
